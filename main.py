be334465cf37df0708bd0b0991535e47d5b69185
Nader Zare <nader.zare88@gmail.com>
2019-10-03 15:53:55 -0300

    first version of vessel gym with discrite action

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+# from car import *
+# env = MountainCarEnv()
+2
 from vessel_gym import *
+env = VesselEnv()
+
+state = env.reset()
+
+done = False
+while not done:
+    env.render()
+    state, reward, done, s = env.step(int(input('enter action:')))
+
+env.close()
+
--- vessel_gym.py
+++ vessel_gym.py
@@ -1,10 +1,16 @@
 import math
-
 import numpy as np
 
 import gym
 from gym import spaces
 from gym.utils import seeding
+
+
+def calc_dist(a, b):
+    print(a, b)
+    dist = math.sqrt(pow(a[0] - b[0], 2) + pow(a[1] - b[1], 2))
+    return dist
+
 
 class VesselEnv(gym.Env):
     metadata = {
@@ -12,23 +18,26 @@ class VesselEnv(gym.Env):
         'video.frames_per_second': 30
     }
 
-    def __init__(self, goal_velocity = 0):
-        self.min_position = -10
-        self.max_position = 10
-        self.max_speed = 1.0
-        self.goal_position = 0.5
-        self.goal_velocity = goal_velocity
-        
-        self.force=0.001
-        self.gravity=0.0025
+    def __init__(self):
+        self.min_x = -0.5
+        self.min_y = -0.5
+        self.max_x = 3
+        self.max_y = 3
 
-        self.low = np.array([self.min_position, -self.max_speed])
-        self.high = np.array([self.max_position, self.max_speed])
+        self.vessel_position = np.array([0., 0.])
+        self.vessel_radius = 0.1
+        self.state = self.vessel_position
+        self.goal_position = np.array([2.5, 2.5])
+        self.goal_radius = 0.1
+        self.obstacles_number = 9
+        self.obstacles_position = [[1.0, 0.5], [0.5, 1.0], [0.75, 1.5], [0.25, 2.25], [1.0, 2.0], [1.5, 1.5],
+                                   [2.0, 0.25], [2.0, 1.0], [2.5, 0]]
+        self.obstacles_radius = 0.1
+        self.max_speed = 0.5
 
         self.viewer = None
-
-        self.action_space = spaces.Discrete(3)
-        self.observation_space = spaces.Box(self.low, self.high, dtype=np.float32)
+        self.action_space = spaces.Discrete(4)
+        self.observation_space = spaces.Box(low=np.array([-10.0, -10.0]), high=np.array([10.0, 10.0]), dtype=np.float32)
 
         self.seed()
 
@@ -39,83 +48,86 @@ class VesselEnv(gym.Env):
     def step(self, action):
         assert self.action_space.contains(action), "%r (%s) invalid" % (action, type(action))
 
-        position, velocity = self.state
-        velocity += (action-1)*self.force + math.cos(3*position)*(-self.gravity)
-        velocity = np.clip(velocity, -self.max_speed, self.max_speed)
-        position += velocity
-        position = np.clip(position, self.min_position, self.max_position)
-        if (position==self.min_position and velocity<0): velocity = 0
+        position = self.state
+        action2velocity = [np.array([self.max_speed, 0.0]),np.array([0.0, self.max_speed]),np.array([-self.max_speed, 0.0]),np.array([0.0, -self.max_speed])]
+        position += action2velocity[action]
 
-        done = bool(position >= self.goal_position and velocity >= self.goal_velocity)
+        done = False
         reward = -1.0
+        if position[0] < self.min_x or position[0] > self.max_x or position[1] < self.min_y or position[1] > self.max_y:
+            done = True
+            reward = -1.0
+        elif self.is_collision_obstacle():
+            done = True
+            reward = -1.0
+        elif self.is_collision_goal():
+            done = True
+            reward = 0.0
 
-        self.state = (position, velocity)
+        self.state = position
         return np.array(self.state), reward, done, {}
 
     def reset(self):
-        self.state = np.array([self.np_random.uniform(low=-0.6, high=-0.4), 0])
+        self.state = self.vessel_position
         return np.array(self.state)
-
-    def _height(self, xs):
-        return np.sin(3 * xs)*.45+.55
 
     def render(self, mode='human'):
         screen_width = 600
-        screen_height = 400
+        screen_height = 600
 
-        world_width = self.max_position - self.min_position
+        world_width = self.max_x - self.min_y
         scale = screen_width/world_width
-        carwidth=40
-        carheight=20
-
 
         if self.viewer is None:
             from gym.envs.classic_control import rendering
             self.viewer = rendering.Viewer(screen_width, screen_height)
-            xs = np.linspace(self.min_position, self.max_position, 100)
-            ys = self._height(xs)
-            xys = list(zip((xs-self.min_position)*scale, ys*scale))
-
-            self.track = rendering.make_polyline(xys)
-            self.track.set_linewidth(4)
-            self.viewer.add_geom(self.track)
-
-            clearance = 10
-
-            l,r,t,b = -carwidth/2, carwidth/2, carheight, 0
-            car = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
-            car.add_attr(rendering.Transform(translation=(0, clearance)))
+            vessel = rendering.make_circle(self.vessel_radius * scale)
+            # vessel.add_attr(rendering.Transform(translation=(0, 0)))
+            vessel.set_color(.0, .0, 1)
             self.cartrans = rendering.Transform()
-            car.add_attr(self.cartrans)
-            self.viewer.add_geom(car)
-            frontwheel = rendering.make_circle(carheight/2.5)
-            frontwheel.set_color(.5, .5, .5)
-            frontwheel.add_attr(rendering.Transform(translation=(carwidth/4,clearance)))
-            frontwheel.add_attr(self.cartrans)
-            self.viewer.add_geom(frontwheel)
-            backwheel = rendering.make_circle(carheight/2.5)
-            backwheel.add_attr(rendering.Transform(translation=(-carwidth/4,clearance)))
-            backwheel.add_attr(self.cartrans)
-            backwheel.set_color(.5, .5, .5)
-            self.viewer.add_geom(backwheel)
-            flagx = (self.goal_position-self.min_position)*scale
-            flagy1 = self._height(self.goal_position)*scale
-            flagy2 = flagy1 + 50
-            flagpole = rendering.Line((flagx, flagy1), (flagx, flagy2))
-            self.viewer.add_geom(flagpole)
-            flag = rendering.FilledPolygon([(flagx, flagy2), (flagx, flagy2-10), (flagx+25, flagy2-5)])
-            flag.set_color(.8,.8,0)
-            self.viewer.add_geom(flag)
+            vessel.add_attr(self.cartrans)
+            self.viewer.add_geom(vessel)
 
-        pos = self.state[0]
-        self.cartrans.set_translation((pos-self.min_position)*scale, self._height(pos)*scale)
-        self.cartrans.set_rotation(math.cos(3 * pos))
+            obstacles = [rendering.make_circle(self.obstacles_radius * scale) for _ in range(self.obstacles_number)]
+            self.obstacles_cp = []
+            for o, pos in zip(obstacles, self.obstacles_position):
+                o.set_color(1, 0, 0)
+                x = (pos[0] - self.min_x) * scale
+                y = (pos[1] - self.min_y) * scale
+                # o.add_attr(rendering.Transform(translation=(x, y)))
+                self.obstacles_cp.append([rendering.Transform(),[x, y]])
+                o.add_attr(self.obstacles_cp[-1][0])
+                self.viewer.add_geom(o)
 
-        return self.viewer.render(return_rgb_array = mode=='rgb_array')
-    
+            target = rendering.make_circle(self.goal_radius * scale)
+            target.set_color(.0, 1, 0)
+            self.target_c = rendering.Transform()
+            target.add_attr(rendering.Transform(translation=(0, 0)))
+            target.add_attr(self.target_c)
+            self.viewer.add_geom(target)
+
+        pos = self.state
+        self.cartrans.set_translation((pos[0] - self.min_x) * scale, (pos[1] - self.min_y) * scale)
+        self.target_c.set_translation((2.5 - self.min_x) * scale, (2.5 - self.min_y) * scale)
+        for c, p in self.obstacles_cp:
+            c.set_translation(p[0], p[1])
+        return self.viewer.render(return_rgb_array=mode == 'rgb_array')
+
     def get_keys_to_action(self):
-        return {():1,(276,):0,(275,):2,(275,276):1} #control with left and right arrow keys 
-    
+        return {():1,(276,):0,(275,):2,(275,276):1} #control with left and right arrow keys
+
+    def is_collision_obstacle(self):
+        collision = lambda obstacle_pos: True if calc_dist(self.vessel_position,
+                                                           obstacle_pos) < self.obstacles_radius + self.vessel_radius else False
+        if any(list(map(collision, self.obstacles_position))):
+            return True
+        return False
+
+    def is_collision_goal(self):
+        if calc_dist(self.vessel_position, self.goal_position) < self.goal_radius + self.vessel_radius:
+            return True
+        return False
+
     def close(self):
         if self.viewer:
             self.viewer.close()
