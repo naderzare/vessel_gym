503d66d17af9c6008c33d69d06fb886fe5355280
Nader Zare <nader.zare88@gmail.com>
2019-10-08 10:12:25 -0300

    add vessel motion formula

--- vessel_gym.py
+++ vessel_gym.py
@@ -7,7 +7,6 @@ from gym.utils import seeding
 
 
 def calc_dist(a, b):
-    print(a, b)
     dist = math.sqrt(pow(a[0] - b[0], 2) + pow(a[1] - b[1], 2))
     return dist
 
@@ -24,9 +23,14 @@ class VesselEnv(gym.Env):
         self.max_x = 3
         self.max_y = 3
 
-        self.vessel_position = np.array([0., 0.])
+        self.vessel_position_x = 0.
+        self.vessel_position_y = 0.
+        self.vessel_head_angle = 0.
+        self.vessel_vel_v = 0.
+        self.vessel_vel_u = 0.
+        self.vessel_vel_r = 0.
         self.vessel_radius = 0.1
-        self.state = self.vessel_position
+        self.state = np.array([self.vessel_position_x,self.vessel_position_y, self.vessel_head_angle, self.vessel_vel_u, self.vessel_vel_v, self.vessel_vel_r])
         self.goal_position = np.array([2.5, 2.5])
         self.goal_radius = 0.1
         self.obstacles_number = 9
@@ -36,7 +40,8 @@ class VesselEnv(gym.Env):
         self.max_speed = 0.5
 
         self.viewer = None
-        self.action_space = spaces.Discrete(4)
+        self.action_space = spaces.Box(low=-1, high=+1,
+                                       shape=(2,), dtype=np.float32)
         self.observation_space = spaces.Box(low=np.array([-10.0, -10.0]), high=np.array([10.0, 10.0]), dtype=np.float32)
 
         self.seed()
@@ -45,16 +50,29 @@ class VesselEnv(gym.Env):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
-    def step(self, action):
-        assert self.action_space.contains(action), "%r (%s) invalid" % (action, type(action))
+    def R(self):
+        res = np.zeros((3,3))
+        res[0][0] = np.cos(self.vessel_head_angle)
+        res[1][1] = np.cos(self.vessel_head_angle)
+        res[0][1] = -np.sin(self.vessel_head_angle)
+        res[1][0] = np.sin(self.vessel_head_angle)
+        res[2][2] = 1
+        return res
 
-        position = self.state
-        action2velocity = [np.array([self.max_speed, 0.0]),np.array([0.0, self.max_speed]),np.array([-self.max_speed, 0.0]),np.array([0.0, -self.max_speed])]
-        position += action2velocity[action]
+    def step(self, action):
+        print(action)
+        dV = np.array([action[0], 0, action[1]])
+        print('dV:', dV)
+        print('R:', self.R())
+        dP = np.dot(self.R(), dV)
+        print('dp', dP)
+        self.vessel_position_x += dP[0]
+        self.vessel_position_y += dP[1]
+        self.vessel_head_angle += dP[2]
 
         done = False
         reward = -1.0
-        if position[0] < self.min_x or position[0] > self.max_x or position[1] < self.min_y or position[1] > self.max_y:
+        if self.vessel_position_x < self.min_x or self.vessel_position_x > self.max_x or self.vessel_position_y < self.min_y or self.vessel_position_y > self.max_y:
             done = True
             reward = -1.0
         elif self.is_collision_obstacle():
@@ -64,12 +82,12 @@ class VesselEnv(gym.Env):
             done = True
             reward = 0.0
 
-        self.state = position
+        self.state = np.array([self.vessel_position_x,self.vessel_position_y, self.vessel_head_angle, self.vessel_vel_u, self.vessel_vel_v, self.vessel_vel_r])
         return np.array(self.state), reward, done, {}
 
     def reset(self):
-        self.state = self.vessel_position
-        return np.array(self.state)
+        self.state = np.array([self.vessel_position_x,self.vessel_position_y, self.vessel_head_angle, self.vessel_vel_u, self.vessel_vel_v, self.vessel_vel_r])
+        return self.state
 
     def render(self, mode='human'):
         screen_width = 600
@@ -81,12 +99,20 @@ class VesselEnv(gym.Env):
         if self.viewer is None:
             from gym.envs.classic_control import rendering
             self.viewer = rendering.Viewer(screen_width, screen_height)
-            vessel = rendering.make_circle(self.vessel_radius * scale)
+
+            xs = [0, 2, 3, 2, 0]
+            ys = [1, 1, 0, -1, -1]
+            for i in range(5):
+                xs[i] *= 10
+                ys[i] *= 10
+            print(xs)
+            xys = list(zip(xs, ys))
+            vessel_circle = rendering.make_polygon(xys)
             # vessel.add_attr(rendering.Transform(translation=(0, 0)))
-            vessel.set_color(.0, .0, 1)
+            vessel_circle.set_color(.0, .0, 1)
             self.cartrans = rendering.Transform()
-            vessel.add_attr(self.cartrans)
-            self.viewer.add_geom(vessel)
+            vessel_circle.add_attr(self.cartrans)
+            self.viewer.add_geom(vessel_circle)
 
             obstacles = [rendering.make_circle(self.obstacles_radius * scale) for _ in range(self.obstacles_number)]
             self.obstacles_cp = []
@@ -106,8 +132,9 @@ class VesselEnv(gym.Env):
             target.add_attr(self.target_c)
             self.viewer.add_geom(target)
 
-        pos = self.state
+        pos = [self.vessel_position_x, self.vessel_position_y]
         self.cartrans.set_translation((pos[0] - self.min_x) * scale, (pos[1] - self.min_y) * scale)
+        self.cartrans.set_rotation(self.vessel_head_angle)
         self.target_c.set_translation((2.5 - self.min_x) * scale, (2.5 - self.min_y) * scale)
         for c, p in self.obstacles_cp:
             c.set_translation(p[0], p[1])
@@ -117,14 +144,14 @@ class VesselEnv(gym.Env):
         return {():1,(276,):0,(275,):2,(275,276):1} #control with left and right arrow keys
 
     def is_collision_obstacle(self):
-        collision = lambda obstacle_pos: True if calc_dist(self.vessel_position,
+        collision = lambda obstacle_pos: True if calc_dist((self.vessel_position_x,self.vessel_position_y),
                                                            obstacle_pos) < self.obstacles_radius + self.vessel_radius else False
         if any(list(map(collision, self.obstacles_position))):
             return True
         return False
 
     def is_collision_goal(self):
-        if calc_dist(self.vessel_position, self.goal_position) < self.goal_radius + self.vessel_radius:
+        if calc_dist((self.vessel_position_x,self.vessel_position_y), self.goal_position) < self.goal_radius + self.vessel_radius:
             return True
         return False
 
