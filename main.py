0b9efd28731f03e9e16f9a446b2cd6fe25922bce
Nader Zare <nader.zare88@gmail.com>
2019-10-08 10:11:54 -0300

    add test move

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 # from car import *
 # env = MountainCarEnv()
-2
+import time
 from vessel_gym import *
 env = VesselEnv()
 
 state = env.reset()
 
 done = False
+
+acts = [(0.2,0.0),(0.2,1),(0.2,0.5),(0.2,-0.5),(0.2,-0.3),(0.2,-0.2),(0.2,0.5),(0.2,0.4),(0.2,0.3),(0.2,0.3),(0.2,-0.3),(0.2,-0.3),(0.2,0.3)]
+acs = 0
 while not done:
     env.render()
-    state, reward, done, s = env.step(int(input('enter action:')))
+    # ac = input()
+    # u, r = ac.split(',')
+    # u = float(u)
+    # r = float(r)
+    time.sleep(0.5)
+    state, reward, done, s = env.step(acts[acs])
+    acs+=1
 
 env.close()
 
